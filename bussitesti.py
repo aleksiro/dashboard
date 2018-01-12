21ebf73a09a788dfed4987d68046ea740287cd47
roima <aleksi.roima@student.tut.fi>
2018-01-13 00:05:05 +0200

    Changed to API that better fits the purpose

--- bussitesti.py
+++ bussitesti.py
@@ -17,14 +17,13 @@ directionRef = 1
 directionRef = 1
 
 """
-# linjat, pysäkit, pysäkkijärjestynro
-wanted_stops = "3723,3577,3737"
+
+import credentials as cre
 
 
-# Haetaan data
 import requests
 import json
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
 # =============================================================================
 # act_url = 'http://data.itsfactory.fi/journeys/api/1/vehicle-activity'
@@ -48,38 +47,104 @@ from datetime import datetime, timedelta
 
 
 
-    
-stop_url = 'http://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=%s' % (wanted_stops)
-stop_response = requests.get(stop_url)
+# =============================================================================
+#     
+# stop_url = 'http://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=%s' % (wanted_stops)
+# stop_response = requests.get(stop_url)
+# 
+# # Linjan nimi, alkuperäinen saapumisaika, ennustettu saapumisaika
+# arrivals = []
+# if (stop_response.ok):
+#     stop_json = json.loads(stop_response.content)
+#     for key, val in stop_json['body'].items():
+#         for bus_line in val: 
+#             if (bus_line['directionRef'] == "1"):
+#                 arrivals.append([bus_line['lineRef']
+#                 , bus_line['call']['aimedArrivalTime']
+#                 , bus_line['call']['expectedArrivalTime']])
+# 
+# for row in arrivals:
+#     scheduled = datetime.strptime(row[1], '%Y-%m-%dT%H:%M:%S+02:00')
+#     expected = datetime.strptime(row[2], '%Y-%m-%dT%H:%M:%S+02:00')
+#     delay = expected - scheduled
+#     print("Bussilinja: ", row[0], 
+#           "Suunniteltu: ", str(scheduled.hour) + ":" + str(scheduled.minute), 
+#           "Arvioitu: ", str(expected.hour) + ":" + str(expected.minute), 
+#           "Myöhästyy: ", str(int(delay.seconds/60)))
+# # Ei saada kuin noin 10 min eteenpäin
+# =============================================================================
 
-# Linjan nimi, alkuperäinen saapumisaika, ennustettu saapumisaika
+
+#api.publictransport.tampere.fi/prod/?request=stop&user=aleksiroima&pass=kjsfdx1954qpo&code=Vanha&format=json
+
+# Kokeillaan toisesta rajapinnasta jos saisi enemmän dataa
+# Haetaan data ja luodaan lista, joka pitää sisällään tupleja, joissa linjan nimi ja saapumisaika pysäkille
+def get_busses(url, arrivals):
+    right_busses = [["3A", "3B", "4", "6"], ["3723", "3577", "3737"]]
+    response = requests.get(url)
+    if (response.ok):
+        json_data = json.loads(response.content)
+        for bus in json_data[0]['departures']:
+            if (bus['direction'] == "1"):
+                if (((json_data[0]['code'] == right_busses[1][0]) and ((bus['code'] == right_busses[0][0]) or (bus['code'] == right_busses[0][1])))
+                or ((json_data[0]['code'] == right_busses[1][1]) and (bus['code'] == right_busses[0][2]))
+                or ((json_data[0]['code'] == right_busses[1][2]) and (bus['code'] == right_busses[0][3]))):
+                    arrivals.append([bus['code'], bus['time']])
+                 
+urls = [ 'http://api.publictransport.tampere.fi/prod/?request=stop&user=%s&pass=%s&code=3723&format=json&dep_limit=20&time_limit=360' % (cre.username, cre.passphrase)
+        , 'http://api.publictransport.tampere.fi/prod/?request=stop&user=%s&pass=%s&code=3577&format=json&dep_limit=20&time_limit=360' % (cre.username, cre.passphrase)
+        , 'http://api.publictransport.tampere.fi/prod/?request=stop&user=%s&pass=%s&code=3737&format=json&dep_limit=20&time_limit=360' % (cre.username, cre.passphrase)]
+
+
 arrivals = []
-if (stop_response.ok):
-    stop_json = json.loads(stop_response.content)
-    for key, val in stop_json['body'].items():
-        for bus_line in val: 
-            if (bus_line['directionRef'] == "1"):
-                arrivals.append([bus_line['lineRef']
-                , bus_line['call']['aimedArrivalTime']
-                , bus_line['call']['expectedArrivalTime']])
+for url in urls:
+    get_busses(url, arrivals)
+    
+# Karsitaan jäljellä vain bussit, jotka lähtevät 5 min - 2 h päästä:
 
-for row in arrivals:
-    scheduled = datetime.strptime(row[1], '%Y-%m-%dT%H:%M:%S+02:00')
-    expected = datetime.strptime(row[2], '%Y-%m-%dT%H:%M:%S+02:00')
-    delay = expected - scheduled
-    print("Bussilinja: ", row[0], 
-          "Suunniteltu: ", str(scheduled.hour) + ":" + str(scheduled.minute), 
-          "Arvioitu: ", str(expected.hour) + ":" + str(expected.minute), 
-          "Myöhästyy: ", str(int(delay.seconds/60)))
+    
+for bus in arrivals:
+    # Koska kello ei mene 26 asti:
+    if (bus[1][0:2] == "24"):
+        bus[1] = bus[1].replace("24", "00", 1)
+    elif (bus[1][0:2] == "25"):
+        bus[1] = bus[1].replace("25", "01", 1)
+    elif (bus[1][0:2] == "26"):
+        bus[1] = bus[1].replace("26", "02", 1)
+    bus[1] = int((datetime.combine(date.min, datetime.strptime(bus[1], '%H%M').time()) - datetime.combine(date.min, datetime.now().time())).total_seconds() / 60)
+    if bus[1] < -1000:
+        bus[1] = 6
+    if ((bus[1] < 5) or (bus[1] > 100)):
+        arrivals.remove(bus)
+
+from operator import itemgetter
+#arrivals = sorted(arrivals, key=itemgetter(1))
+                
+                
 
 
-                
-                
-                
-                
-                
-                
-                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 
                 
                 
